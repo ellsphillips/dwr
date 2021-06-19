16ad62293edcc84b378dfa1609f31710380046c1
ellsphillips <ellsphillipsuni@gmail.com>
2021-06-19 16:24:40 +0100

    Builder subpackage

--- doctor/__init__.py
+++ doctor/__init__.py
@@ -4,8 +4,8 @@ Put on the LaTeX gloves.
 
 from .doctor import *
 
-from .table import TabularBuilder as table
-from .plot import PlotBuilder as plot
+from .builder.table import TabularBuilder as table
+from .builder.plot import PlotBuilder as plot
 
 version_info = (0, 0, 1)
 __version__ = ".".join([str(x) for x in version_info])
