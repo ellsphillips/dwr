9cff1b46e2cce6a9f27bd9010931b40847038e75
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-06-21 11:24:53 +0100

    Options object

--- doctor/utils/tex.py
+++ doctor/utils/tex.py
@@ -8,7 +8,64 @@ class TeXDefaults:
   """
 
   options = {
-    "report_path": r"demo\src"
+    "report_path": r"demo\src",
+    "requirements": {
+      "packages": {
+        "inputenc": ["utf8"],
+        "fontenc": ["T1"],
+        "textcomp": [],
+        "gensymb": [],
+        "babel": ["english"],
+        "ragged2e": ["document"],
+        "float": [],
+        "bm": [],
+        "amsfonts": [],
+        "cancel": [],
+        "varwidth": [],
+        "blindtext": [],
+        "underscore": [],
+        "lipsum": [],
+        "geometry": ["a4paper,left=2.5cm,right=2.5cm,bottom=3cm,top=4cm"],
+        "graphicx": [],
+        "caption": [],
+        "fp": [],
+        "epstopdf": [],
+        "framed": [],
+        "amsmath": [],
+        "textpos": ["absolute"],
+        "fancyhdr": [],
+        "xkeyval": [],
+        "alphalph": [],
+        "ifthen": [],
+        "appendix": [],
+        "titletoc": [],
+        "etoolbox": [],
+        "emptypage": [],
+        "afterpage": [],
+        "hyperref": ["verbose"],
+        "tcolorbox": [],
+        "tikz": [],
+        "tkz-euclide": [],
+        "pgfplots": [],
+        "collcell": [],
+        "booktabs": [],
+        "longtable": [],
+        "multirow": [],
+        "multicol": [],
+        "colortbl": [],
+        "hhline": [],
+        "dcolumn": [],
+        "tabularx": [],
+        "ctable": [],
+        "xltabular": [],
+        "silence": [],
+      },
+      "libraries": [
+        r"\usepgfplotslibrary{fillbetween}",
+        r"\usetikzlibrary{intersections}",
+        r"\pgfplotsset{compat=1.12}",
+      ]
+    }
   }
   
   env_table = r"""
@@ -96,63 +153,6 @@ class TeXDefaults:
         \endxltabular
       }
     """
-
-  required_packages = {
-    "inputenc": ["utf8"],
-    "fontenc": ["T1"],
-    "textcomp": [],
-    "gensymb": [],
-    "babel": ["english"],
-    "ragged2e": ["document"],
-    "float": [],
-    "bm": [],
-    "amsfonts": [],
-    "cancel": [],
-    "varwidth": [],
-    "blindtext": [],
-    "underscore": [],
-    "lipsum": [],
-    "geometry": ["a4paper,left=2.5cm,right=2.5cm,bottom=3cm,top=4cm"],
-    "graphicx": [],
-    "caption": [],
-    "fp": [],
-    "epstopdf": [],
-    "framed": [],
-    "amsmath": [],
-    "textpos": ["absolute"],
-    "fancyhdr": [],
-    "xkeyval": [],
-    "alphalph": [],
-    "ifthen": [],
-    "appendix": [],
-    "titletoc": [],
-    "etoolbox": [],
-    "emptypage": [],
-    "afterpage": [],
-    "hyperref": ["verbose"],
-    "tcolorbox": [],
-    "tikz": [],
-    "tkz-euclide": [],
-    "pgfplots": [],
-    "collcell": [],
-    "booktabs": [],
-    "longtable": [],
-    "multirow": [],
-    "multicol": [],
-    "colortbl": [],
-    "hhline": [],
-    "dcolumn": [],
-    "tabularx": [],
-    "ctable": [],
-    "xltabular": [],
-    "silence": [],
-  }
-
-  additional_options = [
-    r"\usepgfplotslibrary{fillbetween}",
-    r"\usetikzlibrary{intersections}",
-    r"\pgfplotsset{compat=1.12}",
-  ]
 
   pkg_cmd = [r"\usepackage", r"\RequirePackage"]
 
