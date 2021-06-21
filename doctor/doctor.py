bbae1dd0198f75010cb540bbf3204aabc6e7c758
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-06-21 13:23:32 +0100

    Verbose flag for build method

--- doctor/doctor.py
+++ doctor/doctor.py
@@ -8,16 +8,17 @@ if sys.version_info < (3, 6):
   raise RuntimeError("This module requires Python 3.6 or higher")
 
 
-def build():
+def build(verbose=False):
   """
   Run pdflatex shell to build the report.
   """
   shell_cmd = " ".join([
     "pdflatex",
     "-halt-on-error",
+    "" if verbose else "-quiet",
     "-output-directory",
     f"{tex.options['document']['path']}",
     f"{tex.options['document']['name']}.tex"
   ])
-  
+
   subprocess.run(shell_cmd)
