276ad085702982f608845cda1b15c25787cb0b65
ellsphillips <ellsphillipsuni@gmail.com>
2021-08-24 19:56:37 +0100

    Placeholder for Bar class

---
+++ doctor/builder/plot/bar.py
@@ -0,0 +1,13 @@
+class Bar:
+  """
+  """
+  def __init__(self) -> None:
+    pass
+
+  def intro(self) -> None:
+    print(
+      "\033[1m\33[96m",
+      "-> OF BAR-TYPE",
+      "\33[0m",
+      sep=""
+    )
