ab938ad495675be98a4e1d763dd773823c364fa8
Elliott Phillips <ellsphillipsuni@gmail.com>
2021-09-06 15:56:54 +0100

    Template method

--- doctor/builder/plot/bar.py
+++ doctor/builder/plot/bar.py
@@ -8,11 +8,28 @@ class Bar():
   ) -> None:
     pass
 
-  def intro(self) -> None:
-    print(
-      "\033[1m\33[96m",
-      "-> OF BAR-TYPE",
-      "\33[0m",
-      sep=""
-    )
-    return ""
+  @staticmethod
+  def _template() -> None:
+    """
+    \begin{doctor-bar}[%
+      plot type={ybar},
+      data source={src/plots/bar.dat},
+      somebool=false,
+      caption={Hello, world!},
+      label={bar-test},
+      xmin=0,
+      xmax=12,
+      ymin=0,
+      ymax=35,
+    ]%
+      \addplot+[%
+        nodes near coords,
+        point meta=explicit symbolic,
+        mark=none,
+        ons-blue,
+        very thick
+      ] table [x=index, y=values, meta=values]%
+      {\doctordatasource};
+    \end{doctor-bar}
+    """
+    pass
