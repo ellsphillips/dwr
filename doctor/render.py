b376c89d533ece013e7d08bf24813075f46e3e09
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-02-02 12:22:38 +0000

    Pad console outputs

--- doctor/render.py
+++ doctor/render.py
@@ -3,11 +3,12 @@ from .environment import Environment
 
 def render(environment: Environment) -> None:
     print(
+        "\n",
         "\n".join(
             [
                 environment.begin + environment.options,
                 environment.body,
                 environment.end,
             ]
-        )
+        ),
     )
