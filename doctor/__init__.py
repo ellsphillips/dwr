0c9d4ff23b4811af838b037aec9219820539b697
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-02-02 12:23:08 +0000

    Compact module docstring

--- doctor/__init__.py
+++ doctor/__init__.py
@@ -1,11 +1,10 @@
-"""
-Put on the LaTeX gloves.
-"""
+"""Put on the LaTeX gloves."""
 
-from .config.config import read_config as read_config
 from . import data as data
+from .config.config import read_config as read_config
 from .plot.plot import plot as plot
 from .render import render as render
+from .table.table import table as table
 
 version_info = (0, 0, 2)
 __version__ = ".".join([str(x) for x in version_info])
