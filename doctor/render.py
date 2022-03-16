a909a34c86b154d0ef7817a1044d0add6650a0b6
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-03-16 19:48:10 +0000

    Render environments with save placeholder

--- doctor/render.py
+++ doctor/render.py
@@ -1,14 +1,17 @@
 from .environment import Environment
 
 
-def render(environment: Environment) -> None:
-    print(
-        "\n",
-        "\n".join(
-            [
-                environment.begin + environment.options,
-                environment.body,
-                environment.end,
-            ]
-        ),
+def render(environment: Environment) -> str:
+    result = "\n".join(
+        [
+            environment.begin + environment.options,
+            environment.body,
+            environment.end,
+        ]
     )
+    print("\n", result)
+    return result
+
+
+def save(render: str, file_path: str) -> None:
+    print(file_path)
