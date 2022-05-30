14611eef6ac644cad55a5822c8a9b5e3724d291b
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-05-30 15:53:26 +0100

    Package config

--- setup.py
+++ setup.py
@@ -1,45 +1,4 @@
-from setuptools import (
-  setup,
-  find_namespace_packages
-)
+from setuptools import setup
 
-with open(file="README.md", mode="r") as readme_handle:
-  long_description = readme_handle.read()
-
-setup(
-  name='Doctor',
-  author='Elliott Phillips',
-  author_email='ellsphillipsuni@gmail.com',
-  version='0.0.2',
-  description='An automated documentation assistant built in Python and TeX for procedural, data-driven reporting.',
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  url='https://github.com/ellsphillips/doctor',
-  install_requires=[
-    'geojson',
-    'kaleido',
-    'matplotlib',
-    'numpy',
-    'pandas',
-    'Pillow',
-    'plotly',
-  ],
-  keywords='python, latex, report-generator ',
-  packages=find_namespace_packages(
-    where=['doctor', 'doctor.*']
-  ),
-  include_package_data=True,
-  python_requires='>=3.8',
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Intended Audience :: Science/Research',
-    'Intended Audience :: Technical report writers',
-    'License :: MIT License',
-    'Natural Language :: English',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.8',
-  ]
-)
+if __name__ == "__main__":
+    setup()
