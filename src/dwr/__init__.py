130fd813947edeff12689094019ae89aa3a15f5d
Elliott Phillips <ellsphillipsuni@gmail.com>
2022-10-01 20:10:21 +0100

    Dev: prepare release init a priori

--- src/dwr/__init__.py
+++ src/dwr/__init__.py
@@ -1,1 +1,10 @@
-__version__ = '0.1.0'
+"""Put on the LaTeX gloves."""
+
+from dwr import data
+from dwr.config.config import read_config
+from dwr.plot.plot import plot
+from dwr.render import render
+from dwr.table.table import table
+
+version_info = (0, 1, 0)
+__version__ = ".".join([str(x) for x in version_info])
